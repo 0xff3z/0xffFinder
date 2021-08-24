adce509a3042a769baa5ab9122186c571186fb77
0xff3z <m3andgamer@gmail.com>
2021-08-24 16:30:27 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+
+print("Enter Your Host")
+hostname = input()
+
+response = os.system("ping -c 1" + hostname)
+if response == 0:
+    print(hostname + "Good")
+else:
+    print(hostname + "Not Good")
+
