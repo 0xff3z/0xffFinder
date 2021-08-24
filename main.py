b3f4fdd83fff0aa3a47936cec42c0f299d577ada
0xff3z <m3andgamer@gmail.com>
2021-08-24 16:32:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 print("Enter Your Host")
 hostname = input()
 
-response = os.system("ping -c 1" + hostname)
+response = os.system("ping -c 1 " + hostname)
 if response == 0:
     print(hostname + "Good")
 else:
