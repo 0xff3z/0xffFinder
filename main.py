a29141b41edb91bbd2a68b776eb8382f945a39fb
0xff3z <m3andgamer@gmail.com>
2021-08-24 22:47:40 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
+import socket
+import requests
+
 
 Error = '\033[91m'
 Succses = '\033[92m'
@@ -8,7 +10,7 @@ Succses = '\033[92m'
 print("Select One")
 inputUser = input('''
 1 - One Domain
-2 - Multiple Domains
+2 - Comming Soon Multiple Domains
 ''')
 
 if inputUser == "":
@@ -18,30 +20,33 @@ if inputUser == "":
 if inputUser == "1":
     print("Enter Your Host")
     hostname = input()
-    print("Enter Number Of Packets")
-    PacketsNumber = input()
-    response = os.system(f"ping -c {PacketsNumber} " + hostname)
-    if response == 0:
-        print(Succses + hostname + "      ...Ok")
+    ip = socket.gethostbyname(f"{hostname}")
+    newhost = (f"http://{hostname}")
+    status = requests.get(f"{newhost}")
+    statuscode = status.status_code
+    if statuscode == 200:
+        print(Succses + ip, " Status ", statuscode)
     else:
-        print(Error + hostname + "       ..No Response")
+        print(statuscode + "  Not Found")
 
 
-if inputUser == "2":
-    print("Locate Your File Domains : Example Domains.txt")
-    hostNames = open(input())
-    print("Enter Number Of Packets")
-    PacketsNumber = input()
-
-    for hostNames in hostNames:
 
 
-        response = os.system(f"ping -c {PacketsNumber} " + hostNames)
-        if response == 0:
-            print(Succses + hostNames + "      ...Ok")
-            print("--"*50)
-        else:
-            print(Error + hostNames + "     ..No Response")
+
+
+# if inputUser == "2":
+#     print("Locate Your File Domains : Example Domains.txt")
+#     hostNames = open(input())
+#
+#     for hostNames in hostNames:
+#         ip = socket.gethostbyname(f"{hostNames}")
+#         newhost = (f"http://{hostNames}")
+#         status = requests.get(f"{newhost}")
+#         statuscode = status.status_code
+#         if statuscode == 200:
+#             print(Succses + ip, " Status ", statuscode)
+#         else:
+#             print(statuscode + "  Not Found")
 
 
 
