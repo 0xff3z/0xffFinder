773ea40cb1bdd3e3d7e7f8e55a9d93970ee97623
0xff3z <m3andgamer@gmail.com>
2021-08-24 18:38:26 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,48 @@
+
 import os
 
-print("Enter Your Host")
-hostname = input()
 
-response = os.system("ping -c 1 " + hostname)
-if response == 0:
-    print(hostname + "Good")
-else:
-    print(hostname + "Not Good")
+Error = '\033[91m'
+Succses = '\033[92m'
+
+
+
+print("Select One")
+inputUser = input('''
+1 - One Domain
+2 - Multiple Domains
+''')
+
+if inputUser == "":
+    print("Error Select One")
+
+
+if inputUser == "1":
+    print("Enter Your Host")
+    hostname = input()
+    print("Enter Number Of Packets")
+    PacketsNumber = input()
+    response = os.system(f"ping -n {PacketsNumber} " + hostname)
+    if response == 0:
+        print(Succses + hostname + "      ...Ok")
+    else:
+        print(Error + hostname + "       ..No Response")
+
+
+if inputUser == "2":
+    print("Locate Your File Domains : Example Doamins.txt")
+    hostNames = open(input())
+    print("Enter Number Of Packets")
+    PacketsNumber = input()
+    for hostNames in hostNames:
+
+
+        response = os.system(f"ping -n {PacketsNumber} " + hostNames)
+        if response == 0:
+            print(Succses + hostNames + "      ...Ok")
+        else:
+            print(Error + hostNames + "       ..No Response")
+
+
+
 
